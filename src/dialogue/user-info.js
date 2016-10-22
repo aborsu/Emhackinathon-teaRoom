d96654625f5920255124e31b4aa92f5808d90678
Augustin Borsu <a.borsu@gmail.com>
2016-10-22 19:54:46 +0200

    refactor(user): move to its own file

---
+++ src/dialogue/user-info.js
@@ -0,0 +1,91 @@
+const builder = require('botbuilder');
+const models = require('../models')
+
+const calculateIdealCalPerDay = (weight, height, age, sex) => {
+  if (sex === 'F') {
+    return (10 * weight) + (6.25 * height)  + ((5 * age) - 161) * 1.55;
+  } else {
+    return (10 * weight) + (6.25 * height)  + ((5 * age) + 5) * 1.55;
+  }
+}
+
+module.exports = [
+  function (session, args, results, next) {
+    session.dialogData.profile = args || {};
+    if (results.response && session.dialogData.profile.name) {
+      console.log(response)
+      session.dialogData.profile.name = results.response;
+    }
+    if (!session.dialogData.profile.gender) {
+      builder.Prompts.choice(session, "What is your gender (M/F) ?", ["M", "F"]);
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+    if (results.response) {
+      session.dialogData.profile.gender = results.response.entity;
+    }
+    if (!session.dialogData.profile.age) {
+      builder.Prompts.number(session, "What is your age ?");
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+      if (results.response) {
+      session.dialogData.profile.age = results.response;
+    }
+    if (!session.dialogData.profile.weight) {
+      builder.Prompts.number(session, "What is your weight (kg)?");
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+    if (results.response) {
+      session.dialogData.profile.weight = results.response;
+    }
+    if (!session.dialogData.profile.height) {
+      builder.Prompts.number(session, "What is your height (cm) ?");
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+  if (results.response) {
+      session.dialogData.profile.height = results.response;
+    }
+
+    if (!session.dialogData.profile.goal) {
+      const idealCalPerDay = calculateIdealCalPerDay(
+        session.dialogData.profile.weight,
+        session.dialogData.profile.height,
+        session.dialogData.profile.age,
+        session.dialogData.profile.gender)
+      builder.Prompts.number(
+        session,
+        `Your ideal calory intake is ${idealCalPerDay},` +
+        'type enter to accept or another value.');
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+    if (results.response) {
+      session.dialogData.profile.goal = results.response.entity;
+    }
+    //  CREATE THE INEXISTANT USER INFOS
+    models.user.create({
+      firstName: session.userData.user.name,
+      gender: session.dialogData.profile.gender,
+      age: session.dialogData.profile.age,
+      weight: session.dialogData.profile.weight,
+      height: session.dialogData.profile.height,
+      goal: session.dialogData.profile.goal
+    }).then( response => next(response))
+  },
+  function (session, results) {
+    session.endDialogWithResult({response: results})
+  }
+]
--- src/index.js
+++ src/index.js
@@ -19,14 +19,6 @@ const loadDatabase = () => {
     });
 };
 
-const calculateIdealCalPerDay = (weight, height, age, sex) => {
-  if (sex === 'F') {
-    return (10 * weight) + (6.25 * height)  + ((5 * age) - 161) * 1.55;
-  } else {
-    return (10 * weight) + (6.25 * height)  + ((5 * age) + 5) * 1.55;
-  }
-}
-
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -107,92 +99,7 @@ bot.dialog('/connection', [
 	}
 ]);
 
-
 //=========================================================
-// TAKING USER INFORMATION BOT
-//=========================================================
-
-bot.dialog('/user', [
-  function (session, args, results, next) {
-    session.dialogData.profile = args || {};
-    if (results.response && session.dialogData.profile.name) {
-      console.log(response)
-      session.dialogData.profile.name = results.response;
-    }
-    if (!session.dialogData.profile.gender) {
-      builder.Prompts.choice(session, "What is your gender (M/F) ?", ["M", "F"]);
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-    if (results.response) {
-      session.dialogData.profile.gender = results.response.entity;
-    }
-    if (!session.dialogData.profile.age) {
-      builder.Prompts.number(session, "What is your age ?");
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-      if (results.response) {
-      session.dialogData.profile.age = results.response;
-    }
-    if (!session.dialogData.profile.weight) {
-      builder.Prompts.number(session, "What is your weight (kg)?");
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-    if (results.response) {
-      session.dialogData.profile.weight = results.response;
-    }
-    if (!session.dialogData.profile.height) {
-      builder.Prompts.number(session, "What is your height (cm) ?");
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-	if (results.response) {
-      session.dialogData.profile.height = results.response;
-    }
-
-    if (!session.dialogData.profile.goal) {
-      const idealCalPerDay = calculateIdealCalPerDay(
-        session.dialogData.profile.weight,
-        session.dialogData.profile.height,
-        session.dialogData.profile.age,
-        session.dialogData.profile.gender)
-      builder.Prompts.number(
-        session,
-        `Your ideal calory intake is ${idealCalPerDay},` +
-        'type enter to accept or another value.');
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-    if (results.response) {
-      session.dialogData.profile.goal = results.response.entity;
-    }
-    //  CREATE THE INEXISTANT USER INFOS
-    models.user.create({
-      firstName: session.userData.user.name,
-      gender: session.dialogData.profile.gender,
-      age: session.dialogData.profile.age,
-      weight: session.dialogData.profile.weight,
-      height: session.dialogData.profile.height,
-      goal: session.dialogData.profile.goal
-    }).then( response => next(response))
-  },
-  function (session, results) {
-    session.endDialogWithResult({response: results})
-  }
-]);
-
 // Add intent handlers
 
 dialog.matches('CheckCalories', builder.DialogAction.send('You want to get your calories for the day.'));
@@ -260,3 +167,7 @@ dialog.matches('CouldIEat', [
         });
     }
     ]);
+
+// // TAKING USER INFORMATION BOT
+// //=========================================================
+  bot.dialog('/user', userInfoDialogue);
