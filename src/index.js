4df1e48023b614dddd526d78540a4cd43867927b
JonathanNoel <jonathan.noel1@gmail.com>
2016-10-22 17:02:22 +0200

    feat(login): add login dialogue

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ const loadDatabase = () => {
     });
 };
 
+const calculateIdealCalPerDay = (weight, height, age, sex) => {
+  if (sex === 'F') {
+    return (10 * weight) + (6.25 * height)  + ((5 * age) - 161) * 1.55;
+  } else {
+    return (10 * weight) + (6.25 * height)  + ((5 * age) + 5) * 1.55;
+  }
+}
+
 //=========================================================
 // Bot Setup
 //=========================================================
@@ -40,6 +48,134 @@ loadDatabase().then(() => {
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+bot.dialog('/', [
+    function (session) {
+        // ORIGINAL - session.beginDialog('/user', session.userData.profile);
+	session.beginDialog('/connection', session.userData.connectionProfile);
+    },
+    function (session, results, next) {
+      session.send('You are logged as %s', results.firstName);
+      session.send('So? What next?');
+    }
+]);
+
+//=========================================================
+// CONNECTION BOT
+//=========================================================
+
+bot.dialog('/connection', [
+
+  function (session, args, next) {
+		session.userData.user = args || {};
+    if (!session.userData.user.name) {
+      builder.Prompts.text(session, "Hey ! What's your name mate ?");
+    } else {
+      next();
+    }
+	},
+	function (session, results, next) {
+    if (results.response) {
+      session.userData.user.name = results.response;
+    }
+
+    const name = session.userData.user.name;
+    models.user.findOne({
+      where: {
+        firstName: name
+      }
+    }).then(resultsDb => {
+      if (resultsDb === null) {
+        session.beginDialog('/user', session.userData.profile);
+      } else {
+        session.send("Tu existes ! %s", resultsDb.firstName);
+        next(resultsDb);
+      }
+    })
+	}
+]);
+
+
+//=========================================================
+// TAKING USER INFORMATION BOT
+//=========================================================
+
+bot.dialog('/user', [
+  function (session, args, results, next) {
+    session.dialogData.profile = args || {};
+    if (results.response && session.dialogData.profile.name) {
+      console.log(response)
+      session.dialogData.profile.name = results.response;
+    }
+    if (!session.dialogData.profile.gender) {
+      builder.Prompts.choice(session, "What is your gender (M/F) ?", ["M", "F"]);
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+    if (results.response) {
+      session.dialogData.profile.gender = results.response.entity;
+    }
+    if (!session.dialogData.profile.age) {
+      builder.Prompts.number(session, "What is your age ?");
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+      if (results.response) {
+      session.dialogData.profile.age = results.response;
+    }
+    if (!session.dialogData.profile.weight) {
+      builder.Prompts.number(session, "What is your weight (kg)?");
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+    if (results.response) {
+      session.dialogData.profile.weight = results.response;
+    }
+    if (!session.dialogData.profile.height) {
+      builder.Prompts.number(session, "What is your height (cm) ?");
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+	if (results.response) {
+      session.dialogData.profile.height = results.response;
+    }
+
+    if (!session.dialogData.profile.goal) {
+      const idealCalPerDay = calculateIdealCalPerDay(
+        session.dialogData.profile.weight,
+        session.dialogData.profile.height,
+        session.dialogData.profile.age,
+        session.dialogData.profile.gender)
+      builder.Prompts.number(
+        session,
+        `Your ideal calory intake is ${idealCalPerDay},` +
+        'type enter to accept or another value.');
+    } else {
+      next();
+    }
+  },
+  function (session, results, next) {
+    if (results.response) {
+      session.dialogData.profile.goal = results.response.entity;
+    }
+    //  CREATE THE INEXISTANT USER INFOS
+    models.user.create({
+      firstName: session.userData.user.name,
+      gender: session.dialogData.profile.gender,
+      age: session.dialogData.profile.age,
+      weight: session.dialogData.profile.weight,
+      height: session.dialogData.profile.height,
+      goal: session.dialogData.profile.goal
+    }).then( response => next(response))
+  },
+  function (session, results) {
+    session.endDialogWithResult({response: results})
+  }
+]);
