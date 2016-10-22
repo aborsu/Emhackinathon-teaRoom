43e14f1f71eb9ae8102acb0261d57589eae699bc
Augustin Borsu <a.borsu@gmail.com>
2016-10-22 19:54:46 +0200

    refactor(user): move to its own file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const BPromise = require('bluebird');
 var restify = require('restify');
 var builder = require('botbuilder');
-var _ = require('lodash') 
+var _ = require('lodash')
 const models = require('./models')
 const fatsecret = require('./controllers/fatsecret')
+const userInfoDialogue = require('./dialogue/user-info')
 
 const loadDatabase = () => {
   console.log('DATABASE_LOADING');
@@ -16,14 +17,6 @@ const loadDatabase = () => {
       process.exit(-1);
     });
 };
-
-const calculateIdealCalPerDay = (weight, height, age, sex) => {
-  if (sex === 'F') {
-    return (10 * weight) + (6.25 * height)  + ((5 * age) - 161) * 1.55;
-  } else {
-    return (10 * weight) + (6.25 * height)  + ((5 * age) + 5) * 1.55;
-  }
-}
 
 //=========================================================
 // Bot Setup
@@ -104,92 +97,7 @@ bot.dialog('/connection', [
 	}
 ]);
 
-
 //=========================================================
-// TAKING USER INFORMATION BOT
-//=========================================================
-
-bot.dialog('/user', [
-  function (session, args, results, next) {
-    session.dialogData.profile = args || {};
-    if (results.response && session.dialogData.profile.name) {
-      console.log(response)
-      session.dialogData.profile.name = results.response;
-    }
-    if (!session.dialogData.profile.gender) {
-      builder.Prompts.choice(session, "What is your gender (M/F) ?", ["M", "F"]);
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-    if (results.response) {
-      session.dialogData.profile.gender = results.response.entity;
-    }
-    if (!session.dialogData.profile.age) {
-      builder.Prompts.number(session, "What is your age ?");
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-      if (results.response) {
-      session.dialogData.profile.age = results.response;
-    }
-    if (!session.dialogData.profile.weight) {
-      builder.Prompts.number(session, "What is your weight (kg)?");
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-    if (results.response) {
-      session.dialogData.profile.weight = results.response;
-    }
-    if (!session.dialogData.profile.height) {
-      builder.Prompts.number(session, "What is your height (cm) ?");
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-	if (results.response) {
-      session.dialogData.profile.height = results.response;
-    }
-
-    if (!session.dialogData.profile.goal) {
-      const idealCalPerDay = calculateIdealCalPerDay(
-        session.dialogData.profile.weight,
-        session.dialogData.profile.height,
-        session.dialogData.profile.age,
-        session.dialogData.profile.gender)
-      builder.Prompts.number(
-        session,
-        `Your ideal calory intake is ${idealCalPerDay},` +
-        'type enter to accept or another value.');
-    } else {
-      next();
-    }
-  },
-  function (session, results, next) {
-    if (results.response) {
-      session.dialogData.profile.goal = results.response.entity;
-    }
-    //  CREATE THE INEXISTANT USER INFOS
-    models.user.create({
-      firstName: session.userData.user.name,
-      gender: session.dialogData.profile.gender,
-      age: session.dialogData.profile.age,
-      weight: session.dialogData.profile.weight,
-      height: session.dialogData.profile.height,
-      goal: session.dialogData.profile.goal
-    }).then( response => next(response))
-  },
-  function (session, results) {
-    session.endDialogWithResult({response: results})
-  }
-]);
-
 // Add intent handlers
 
 dialog.matches('CheckCalories', builder.DialogAction.send('You want to get your calories for the day.'));
@@ -199,15 +107,15 @@ dialog.onDefault(builder.DialogAction.send("Could you please rephrase what you j
 //intent handler for adding foods.
 dialog.matches('AddFoods', [
     function (session, args, next) {
-        // console.log(JSON.stringify(args));        
+        // console.log(JSON.stringify(args));
         // console.log(_.filter(args.compositeEntities, entity=>true))
        var foods = _.filter(args.entities, entity => entity.type === 'Food');
        console.log(foods)
        if(foods.length==0)
        {
-         session.send('Sorry, I could not determine which foods you just ate.'); 
+         session.send('Sorry, I could not determine which foods you just ate.');
        }
-       
+
        BPromise.mapSeries(foods, food => {
          fatsecret.food.search(food.entity)
            .then( foodItems => {
@@ -218,20 +126,20 @@ dialog.matches('AddFoods', [
                session.send(foodItems.foods.food[0].food_description);
                session.send(JSON.stringify(foodItems.foods.food[0]));
                //"Per 972g - Calories: 1225kcal | Fat: 33.44g | Carbs: 3.21g | Protein: 213.27g"
-               //we need to extract the calories from the description string               
+               //we need to extract the calories from the description string
                var foodCalories = parseInt(foodItems.foods.food[0].food_description.split('-')[1].split('|')[0].split(':')[1].replace("kcal",' '),10);
-               
+
              }
              else
              {
                session.send("Sorry I could not find any food with that name.");
              }
-             
+
            })
             //process each food with the api.
             //session.send('That sounds delicious!');
             //check how he's doing with the calories
-            //session.send('Your food journal has been updated, keep up the good work!');         
+            //session.send('Your food journal has been updated, keep up the good work!');
        })
     }
     ]);
@@ -245,7 +153,7 @@ dialog.matches('CouldIEat', [
        var foods = _.filter(args.entities, entity => entity.parentType === 'CompositeFood');
        if(foods.length==0)
        {
-         session.send('Sorry, I could not determine which foods you just ate.'); 
+         session.send('Sorry, I could not determine which foods you just ate.');
        }
        session.send(foods.length);
        foods.forEach(function(i)
@@ -257,3 +165,7 @@ dialog.matches('CouldIEat', [
         });
     }
     ]);
+
+// // TAKING USER INFORMATION BOT
+// //=========================================================
+  bot.dialog('/user', userInfoDialogue);
