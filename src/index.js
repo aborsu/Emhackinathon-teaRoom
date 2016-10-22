a290dd42854a42581c346ace16dba4c0b104f77f
Oscar Corredor <oskar132@gmail.com>
2016-10-22 11:40:34 +0200

    added lodash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-
+var _ = require('lodash') 
 //=========================================================
 // Bot Setup
 //=========================================================
