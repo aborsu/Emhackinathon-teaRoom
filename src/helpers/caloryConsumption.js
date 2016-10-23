f635921f7c655129e2fc50ce903bbfe25fc39b59
Augustin Borsu <a.borsu@gmail.com>
2016-10-23 10:51:51 +0200

    feat(caloryConsumption): add calories for range

---
+++ src/helpers/caloryConsumption.js
@@ -0,0 +1,33 @@
+const _ = require('lodash')
+const models = require('../models')
+
+const getCaloryConsumption = (userId, start, end) => {
+  // If end is null, will take the whole start.
+  let startDate, endDate;
+  if (!end) {
+    startDate = start.setHours(2);
+    endDate = new Date(startDate);
+    endDate.setDate(start.getDate() + 1);
+  } else {
+    startDate = start;
+    endDate = end;
+  }
+
+  return models.food.findAll({
+    attributes: ['calories'],
+    where: {
+      userId: userId,
+        createdAt: {
+          $lt: endDate,
+          $gt: startDate
+        }
+      }
+    }).then(
+      results => {
+        return _.reduce(results, (sum, result) => sum + result.calories, 0)
+      })
+}
+
+module.exports = {
+  getCaloryConsumption
+}
